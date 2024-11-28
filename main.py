4ff4cf6b785883a25e9c614146847c47eaa5673d
llane12 <lukelane1@gmail.com>
2024-11-28 10:34:48 +0900

    Added a method to draw a line between the centre of two maze cells

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from maze import *
 def main():
     win = Window(800, 600)
 
-    cell_width = 50
-    line_colors = [ "black", "red", "green", "blue" ]
-    i = 0
-    for y in range(50, 500, cell_width):
-        for x in range(50, 500, cell_width):
-            cell = Cell(x % 50 == 0, y % 100 == 0, x % 100 == 0, y % 50 == 0, Point(x, y), Point(x + cell_width, y + cell_width))
-            cell.draw(win, line_colors[i])
-            i = (i + 1) % len(line_colors)
+    cells = [
+        Cell(True, False, True, True, Point(50, 50), Point(100, 100)),
+        Cell(False, True, True, False, Point(100, 50), Point(150, 100)),
+        Cell(True, True, False, True, Point(100, 100), Point(150, 150))
+    ]
+    for cell in cells:
+        cell.draw(win)
+    draw_move(win, cells[0], cells[1])
+    draw_move(win, cells[1], cells[2], undo=True)
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -6,15 +6,25 @@ class Cell:
         self.has_right_wall = has_right_wall
         self.has_top_wall = has_top_wall
         self.has_bottom_wall = has_bottom_wall
-        self._p1 = p1
-        self._p2 = p2
+        self.p1 = p1
+        self.p2 = p2
 
-    def draw(self, window, line_color):
+    def draw(self, window, line_color="black"):
         if self.has_left_wall:
-            window.draw_line(Line(Point(self._p1.x, self._p1.y), Point(self._p1.x, self._p2.y)), line_color)
+            window.draw_line(Line(Point(self.p1.x, self.p1.y), Point(self.p1.x, self.p2.y)), line_color)
         if self.has_right_wall:
-            window.draw_line(Line(Point(self._p2.x, self._p1.y), Point(self._p2.x, self._p2.y)), line_color)
+            window.draw_line(Line(Point(self.p2.x, self.p1.y), Point(self.p2.x, self.p2.y)), line_color)
         if self.has_top_wall:
-            window.draw_line(Line(Point(self._p1.x, self._p1.y), Point(self._p2.x, self._p1.y)), line_color)
+            window.draw_line(Line(Point(self.p1.x, self.p1.y), Point(self.p2.x, self.p1.y)), line_color)
         if self.has_bottom_wall:
-            window.draw_line(Line(Point(self._p1.x, self._p2.y), Point(self._p2.x, self._p2.y)), line_color)
+            window.draw_line(Line(Point(self.p1.x, self.p2.y), Point(self.p2.x, self.p2.y)), line_color)
+
+def draw_move(window : Window, from_cell, to_cell, undo=False):
+    from_centre = get_cell_centre(from_cell)
+    to_centre = get_cell_centre(to_cell)
+    window.draw_line(Line(from_centre, to_centre), "gray" if undo else "red")
+
+def get_cell_centre(cell):
+    x = (cell.p1.x + cell.p2.x) / 2
+    y = (cell.p1.y + cell.p2.y) / 2
+    return Point(x, y)
